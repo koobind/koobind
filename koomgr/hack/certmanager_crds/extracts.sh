7b29c1933e75812d9b9ed1fd5e5e0a72c7ebc51c
Serge ALEXANDRE <sa@broadsoftware.com>
2020-05-08 12:45:16 +0200

    Move away private stuff

--- koomgr/hack/certmanager_crds/extracts.sh
+++
@@ -1,2 +0,0 @@
-
-kubectl get customresourcedefinitions | grep cert-manager | cut -f 1 -d " " | while read a; do echo "---";  kubectl get customresourcedefinitions $a -o yaml ;done
--- koomgr/rundev.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $MYDIR
-set -e
-make manager
-export KUBECONFIG=${MYDIR}/rundev/kubeconfig
-CMD="bin/manager  --config ${MYDIR}/rundev/config.yml --webhookCertDir ${MYDIR}/config/overlays/dev/cert --webhookHost localhost $@"
-echo $CMD
-$CMD
